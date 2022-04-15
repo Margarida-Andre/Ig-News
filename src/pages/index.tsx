488cd7dc838e487fde1b702f85e347b10774f461
Margarida André <margaridaandre034@gmail.com>
2022-04-15 20:30:46 +0100

    Se aceite, finaliza estrutura da aplicação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
-import styles from "../styles/home.module.scss";
+import Head from "next/head";
 
 export default function Home() {
-  return <h1 className={styles.title}>Hello</h1>;
+  return (
+    <>
+      <Head>
+        <title>home | ig.news </title>
+      </Head>
+      <h1>
+        Hello
+        <span> World</span>
+      </h1>
+    </>
+  );
 }
